77ad6043b6f6f467b87dd5cf44b5a19e29ed912f
Conor McGannon <conor.mcgannon@kellogg.northwestern.edu>
2025-01-28 21:10:11 +0000

    complete 4 hashes (has a few errors at the very end)

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -4,6 +4,33 @@
 
 # Hashes are lists of *key-value pairs*
 
+me = { "name" => "Conor", "location" => "Chicago", "status" => "in class"}
+puts me
+
 # Accessing data from the hash
 
+location = me["location"] #accesses specific data from the array
+p location
+
 # More Complex Hashes
+
+my_profile = {
+    "name" => "Conor", # use "=>" to define a key valuable WITHIN a hash only
+    "location" => {"city" => "Chicago", "state" => "Illinois"},
+    "status" => "in class"
+}
+
+puts my_profile #shows full array
+puts my_profile ["location"]["state"] #looks for a specific value (state) in a specific list (location
+
+my_profile ["name"] = "Conor McGannon" #changes value for variable in an array
+puts my_profile
+
+complete_profile = {"name" => "Conor McGannon", # use "=>" to define a key valuable WITHIN a hash only
+    "location" => {"city" => "Chicago", "state" => "Illinois"},
+    "timeline" => [{"status" => "Eating tacos", "posted" => "7:00AM"},
+                    "status" => "walking", "posted" => "7:30AM"}
+
+puts complete_profile ["timeline"] [0] [status]
+
+# CAN ALSO USE COLONS FOR EACH OF THE ABOVE
