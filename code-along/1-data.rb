3504373a7da4a840dc8329b73bb542f20db50132
Conor McGannon <conor.mcgannon@kellogg.northwestern.edu>
2025-01-28 20:31:36 +0000

    Transfer from other workspace

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,44 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 2
+puts 5
+puts 5+2
 
 # Non-numbers
-
+puts "hello world"
+puts "tacos are" + " delish"
+puts "tacos " * 3
+puts "tacos:" + 3.to_s
 # True, False
+puts true
+puts false
 
 # Nothing
 
+puts nil
+
 # Variables
+
+food = "tacos "
+quantity = 3
+
+puts food * quantity
+
 
 # Combine strings and variables
 
+puts "tacos: #{quantity}"
+puts "#{food}: #{quantity}"
+
 # String manipulation
+ 
+puts "Hello".length #counts letters
+puts "how are you today?".capitalize #capitalizes first letter
+puts "computer".reverse #puts stuff in reverse
+
+creed = "this is the way"
+
+puts creed.upcase #makes everything uppercase
+puts creed.downcase #makes everything lowercase
+puts creed.swapcase #swaps the case of each letter
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -4,12 +4,66 @@
 
 # Booleans
 
+test_is_true = true
+puts test_is_true 
+
+test_is_false = false
+puts test_is_false
+
+
 # Boolean Expressions
+
+puts 3 == 2 #double equal sign EVALUATES 2 variables
+puts 3 != 2
+puts 3 > 2
+puts 3 < 2
 
 # If Conditional Logic
 
+if 3 == 2
+    puts "This text should never display!" #this text will not be displayed because 3 will never = 2
+end
+
+if 3 > 2
+    puts "This text should always display." #this text will get displayed because 3 always > 2
+end
+
 # If/Else Conditional Logic
+
+if 3 == 2
+    puts "This text should never display!" #this text will not be displayed because 3 will never = 2
+else # GETS DISPLAYED IF THE PREVIOUS STATEMENT IS NOT TRUE
+    puts "This text should always displayyy"
+end
+
+user_entered_password = "tacos"
+real_password = "secret"
+
+if user_entered_password == real_password
+    puts "Your password matches. Welcome back."
+else
+    puts "Wrong password. Try again."
+end
 
 # Elsif Conditional Logic
 
+your_team_score = 2
+other_team_score = 2
+if your_team_score > other_team_score
+    puts "You won!"
+elsif your_team_score = other_team_score #stacks if statements if you need to check more than 2 conditions
+    puts "Tied"
+else
+    puts "Your team lost"
+end
+
 # Combining Expressions
+
+temp = 68
+precipitation = 0
+
+if temp >= 65 && temp <= 75 && precipitation == 0 #if the temp is greater than 65 but less than 75, print this statement
+    puts "It's perfect outside." #Stringing && together combines variables. You use single equal sign to assign variables. Double equal sign evaluates variables
+end
+
+# using || instead of && means "OR" instead of "AND"
